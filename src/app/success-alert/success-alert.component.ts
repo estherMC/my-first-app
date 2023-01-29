82f2dea7cf940bd940a23570e2e7feb322a58bc4
Esther Martorell <ster.cat@gmail.com>
2023-01-29 07:09:47 +0100

    :lipstick: Beautify alerts

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'success-alert',
   templateUrl: './success-alert.component.html',
-  styles: ['h3 { color: green }']
+  styleUrls: ['./success-alert.component.css']
+  // styles: ['h3 { color: green }']
 })
 export class SuccessAlertComponent {
 
