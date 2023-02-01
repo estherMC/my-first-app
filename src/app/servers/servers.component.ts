017a03ccc79a3a8fcd9f7a9c73135efcd51a572f
Esther Martorell <ster.cat@gmail.com>
2023-02-01 21:34:29 +0100

    :sparkles: Add property binding example

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -8,6 +8,17 @@ import { Component } from '@angular/core';
   //  template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  allowNewServer = false;
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000);
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
