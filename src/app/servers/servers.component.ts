9c5c8a13a5185b6b64200c72451ff9fa7c7c7db4
Esther Martorell <ster.cat@gmail.com>
2023-02-04 16:25:54 +0100

    :tada: Combine all forms of databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit{
   allowNewServer = false;
   serverCreationStatus = "No server was created!";
-  serverName = 'TestServer';
+  serverName = '';
 
   constructor(){
     setTimeout(() => {
@@ -24,7 +24,7 @@ export class ServersComponent implements OnInit{
   }
 
   onCreateServer(){
-    this.serverCreationStatus = "Server was created!";
+    this.serverCreationStatus = "Server was created! Name of server: " + this.serverName;
   }
 
   onUpdateServerName(event: Event){
